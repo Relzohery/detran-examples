4f779f294bfca8fce66c1b437d628a9422982034
Relzohery <rabab.elzohery@gmail.com>
2020-07-26 14:45:51 -0500

    added perturbation to cross section to generate snapshots

--- slab_reactor/slab_reactor.py
+++ slab_reactor/slab_reactor.py
@@ -28,8 +28,11 @@ from detran import *
 import slab_reactor_materials
 import slab_reactor_geometry
 import time
+import pickle
+np.random.seed(1234)
 
-def run() :
+
+def run(perturb=0, equation = 'transport') :
 
   #------------------------------------------------------------------------------#
   # Initialize
@@ -49,41 +52,101 @@ def run() :
   inp.put_int("inner_max_iters",            1000)
   inp.put_dbl("inner_tolerance",            1e-7)
   inp.put_int("inner_print_level",          0)
-  inp.put_str("outer_solver",               "GS")
+  inp.put_str("outer_solver",               "GMRES")
   inp.put_int("outer_max_iters",            1000)
   inp.put_dbl("outer_tolerance",            1e-7)
   inp.put_int("outer_print_level",          0)
-  inp.put_str("eigen_solver",               "PI")
+  inp.put_str("eigen_solver",               "arnoldi")
   inp.put_int("eigen_max_iters",            200)
   inp.put_dbl("eigen_tolerance",            1e-7)
   inp.put_str("bc_west",                    "vacuum")
   inp.put_str("bc_east",                    "vacuum")
-  inp.put_str("quad_type",                  "gausslegendre")
+  inp.put_str("quad_type",                  "gl")
   inp.put_int("quad_number_polar_octant",   16)
 
-  #------------------------------------------------------------------------------#
-  # Material
-  #------------------------------------------------------------------------------#
-
-  mat = slab_reactor_materials.get_materials()
-
-  #------------------------------------------------------------------------------#
+  
   # Mesh
   #------------------------------------------------------------------------------#
 
   # Options are: assemblyX for X=0,1,2,3 or coreX for X=0,1,2
   mesh = slab_reactor_geometry.get_mesh("core0")
+    
 
-  #------------------------------------------------------------------------------#
-  # Solve
-  #------------------------------------------------------------------------------#
+      
+      
+  
+  
+  if perturb:
+      samples = 100
+      k = []
+      flux_0_samples = np.zeros((140, samples))
+      flux_1_samples = np.zeros((140, samples))
+      fission_density_samples = np.zeros((140, samples))
 
-  start = time.time()
-  solver = Eigen1D(inp, mat, mesh)
-  solver.solve()
-  elapsed = (time.time() - start)
-  print elapsed, " seconds"
+      if equation == "transport":  
+          fname = "results_core0_transport";
+          
+      elif equation == "diffusion":
+        inp.put_str("equation",                   "diffusion")
+        fname = "results_core0_diff"
+        
+
+      for sample in range(samples):
+          print 'sample {}'.format(sample)
+          mat = slab_reactor_materials.get_materials(perturb=perturb)
+          mat.display()
+             
+    
+          #------------------------------------------------------------------------------#
+    
+          #------------------------------------------------------------------------------#
+          # Solve
+          #------------------------------------------------------------------------------#
+    
+          start = time.time()
+          solver = Eigen1D(inp, mat, mesh)
+          solver.solve()
+          elapsed = (time.time() - start)
+          print elapsed, " seconds"
+          print "***************************"
+      
+          
+          k.append(solver.state().eigenvalue())
+          fission_den = solver.fissionsource().density()
+          fission_density_sample = []
+          
+    
+          
+          flux_0 = solver.state().phi(0)
+          flux_0_sample = []
+          
+          flux_1 = solver.state().phi(1)
+          flux_1_sample = []
+          
+          print len(flux_0)
+    
+          
+          for i in range(len(fission_den)):
+              fission_density_sample.append(fission_den[i])          
+          
+          for i in range(len(flux_0)):
+              flux_0_sample.append(flux_0[i])
+              flux_1_sample.append(flux_1[i])
+              
+          fission_density_samples[:, sample] = fission_density_sample
+          flux_0_samples[:, sample] = flux_0_sample
+          flux_1_samples[:, sample] = flux_1_sample
+              
+      data = {'fission_density' : fission_density_samples, 
+              'flux_0': flux_0_samples,
+              'flux_1': flux_1_samples,
+              'k_eff': np.array(k)}
+    
+          
+      pickle.dump(data, open(fname + '.p', 'wb'))
+  
+
 
 if __name__ == "__main__":
   Manager.initialize(sys.argv)
-  run()
+  run(perturb=1,  equation = 'diffusion')
--- slab_reactor/slab_reactor_materials.py
+++ slab_reactor/slab_reactor_materials.py
@@ -7,8 +7,14 @@
 
 from detran import *
 
-def get_materials() :
+
+def get_materials(perturb = 0) :
     # Two-group data from 1-d coarse mesh benchmarks (Mosher, Ilas etc.)
+    
+    if perturb == 0:
+        factor = 0
+    else:
+        factor = np.random.rand()
     
     # Create the Materials object.
     mat = Material.Create(4, 2, "slabreactor");
@@ -18,8 +24,8 @@ def get_materials() :
     # ---------------------------
 
     # Total
-    mat.set_sigma_t(0, 0, 0.1890);       # (obj, matid, g, value);
-    mat.set_sigma_t(0, 1, 1.4633);       
+    mat.set_sigma_t(0, 0, 0.1890*(1 + factor));       # (obj, matid, g, value);
+    mat.set_sigma_t(0, 1, 1.4633*(1 + factor));       
 
     # Fission 
     mat.set_sigma_f(0, 0, 0.0);         # Note, default is zero
@@ -28,70 +34,76 @@ def get_materials() :
     mat.set_chi(0, 1, 0.0);        
 
     # Scattering
-    mat.set_sigma_s(0, 0, 0, 0.1507);    # 1 <- 1
-    mat.set_sigma_s(0, 0, 1, 0.0000);    # 1 <- 2
-    mat.set_sigma_s(0, 1, 0, 0.0380);    # 2 <- 1
-    mat.set_sigma_s(0, 1, 1, 1.4536);    # 2 <- 2
+    mat.set_sigma_s(0, 0, 0, 0.1507*(1 + factor));    # 1 <- 1
+    mat.set_sigma_s(0, 0, 1, 0.0000*(1 + factor));    # 1 <- 2
+    mat.set_sigma_s(0, 1, 0, 0.0380*(1 + factor));    # 2 <- 1
+    mat.set_sigma_s(0, 1, 1, 1.4536*(1 + factor));    # 2 <- 2
+    mat.compute_diff_coef();
 
     # ---------------------------
     # Material 1: Fuel I           
     # ---------------------------
 
     # Total
-    mat.set_sigma_t(1, 0, 0.2263);       # (obj, matid, g, value);
-    mat.set_sigma_t(1, 1, 1.0119);       
+    mat.set_sigma_t(1, 0, 0.2263*(1 + factor));       # (obj, matid, g, value);
+    mat.set_sigma_t(1, 1, 1.0119*(1 + factor));       
 
     # Fission 
-    mat.set_sigma_f(1, 0, 0.0067);
-    mat.set_sigma_f(1, 1, 0.1241);   
+    mat.set_sigma_f(1, 0, 0.0067*(1 + factor));
+    mat.set_sigma_f(1, 1, 0.1241*(1 + factor));   
     mat.set_chi(1, 0, 1.0); 
     mat.set_chi(1, 1, 0.0);        
 
     # Scattering
-    mat.set_sigma_s(1, 0, 0, 0.2006);    # 1 <- 1
-    mat.set_sigma_s(1, 0, 1, 0.0000);    # 1 <- 2
-    mat.set_sigma_s(1, 1, 0, 0.0161);    # 2 <- 1
-    mat.set_sigma_s(1, 1, 1, 0.9355);    # 2 <- 2
+    mat.set_sigma_s(1, 0, 0, 0.2006*(1 + factor));    # 1 <- 1
+    mat.set_sigma_s(1, 0, 1, 0.0000*(1 + factor));    # 1 <- 2
+    mat.set_sigma_s(1, 1, 0, 0.0161*(1 + factor));    # 2 <- 1
+    mat.set_sigma_s(1, 1, 1, 0.9355*(1 + factor));    # 2 <- 2
+    mat.compute_diff_coef();
 
     # ---------------------------
     # Material 3: Fuel II          
     # ---------------------------
 
     # Total
-    mat.set_sigma_t(2, 0, 0.2252);       # (obj, matid, g, value);
-    mat.set_sigma_t(2, 1, 0.9915);       
+    mat.set_sigma_t(2, 0, 0.2252*(1 + factor));       # (obj, matid, g, value);
+    mat.set_sigma_t(2, 1, 0.9915*(1 + factor));       
 
     # Fission 
-    mat.set_sigma_f(2, 0, 0.0078);
-    mat.set_sigma_f(2, 1, 0.1542);   
+    mat.set_sigma_f(2, 0, 0.0078*(1 + factor));
+    mat.set_sigma_f(2, 1, 0.1542*(1 + factor));   
     mat.set_chi(2, 0, 1.0); 
-    mat.set_chi(2, 1, 0.0);        
+    mat.set_chi(2, 1, 0.0); 
 
     # Scattering
-    mat.set_sigma_s(2, 0, 0, 0.1995);    # 1 <- 1
-    mat.set_sigma_s(2, 0, 1, 0.0000);    # 1 <- 2
-    mat.set_sigma_s(2, 1, 0, 0.0156);    # 2 <- 1
-    mat.set_sigma_s(2, 1, 1, 0.9014);    # 2 <- 2
+    mat.set_sigma_s(2, 0, 0, 0.1995*(1 + factor));    # 1 <- 1
+    mat.set_sigma_s(2, 0, 1, 0.0000*(1 + factor));    # 1 <- 2
+    mat.set_sigma_s(2, 1, 0, 0.0156*(1 + factor));    # 2 <- 1
+    mat.set_sigma_s(2, 1, 1, 0.9014*(1 + factor));    # 2 <- 2
+    mat.compute_diff_coef();       
+
 
     # ---------------------------
     # Material 4: Fuel II + Gd          
     # ---------------------------
     
     # Total
-    mat.set_sigma_t(3, 0, 0.2173);       # (obj, matid, g, value);
-    mat.set_sigma_t(3, 1, 1.0606);       
+    mat.set_sigma_t(3, 0, 0.2173*(1 + factor));       # (obj, matid, g, value);
+    mat.set_sigma_t(3, 1, 1.0606*(1 + factor));       
 
     # Fission 
-    mat.set_sigma_f(3, 0, 0.0056);
-    mat.set_sigma_f(3, 1, 0.0187);   
+    mat.set_sigma_f(3, 0, 0.0056*(1 + factor));
+    mat.set_sigma_f(3, 1, 0.0187*(1 + factor));   
     mat.set_chi(3, 0, 1.0); 
     mat.set_chi(3, 1, 0.0);        
 
     # Scattering
-    mat.set_sigma_s(3, 0, 0, 0.1902);	   # 1 <- 1
-    mat.set_sigma_s(3, 0, 1, 0.0000);    # 1 <- 2
-    mat.set_sigma_s(3, 1, 0, 0.0136);    # 2 <- 1
-    mat.set_sigma_s(3, 1, 1, 0.5733);    # 2 <- 2
+    mat.set_sigma_s(3, 0, 0, 0.1902*(1 + factor));	  # 1 <- 1
+    mat.set_sigma_s(3, 0, 1, 0.0000*(1 + factor));    # 1 <- 2
+    mat.set_sigma_s(3, 1, 0, 0.0136*(1 + factor));    # 2 <- 1
+    mat.set_sigma_s(3, 1, 1, 0.5733*(1 + factor));    # 2 <- 2
+    mat.compute_diff_coef();       
+
 
     # ---------------------------
     # FINALIZE     
@@ -100,4 +112,3 @@ def get_materials() :
     mat.finalize();
 
     return mat
-
